14f260367e1f355a8e4fdce36f1c5748edf3dbb3
xaeeedreza <134656430+xaeeedreza@users.noreply.github.com>
2024-06-08 21:43:18 +0330

    _worker.js

---
+++ _worker.js
@@ -0,0 +1,11 @@
+export default {
+  async fetch(request, env) {
+    let url = new URL(request.url);
+    if (url.pathname.startsWith('/')) {
+      url.hostname = '9tyx6m-8080.csb.app/'
+      let new_request = new Request(url, request);
+      return fetch(new_request);
+    }
+    return env.ASSETS.fetch(request);
+  },
+};
